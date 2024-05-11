ce2b64a61ef873e406b8dfeb6e23c531ddfff1b1
mizanurt1 <mizanurt3@gmail.com>
2024-05-11 18:14:28 +0600

    first commit

---
+++ app/todos/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { useQuery, useMutation , useQueryClient } from "@tanstack/react-query"
+export default function Page() {
+
+    const mutation=useMutation({
+        
+    })
+    
+
+    return (
+        <>
+        <h1>Todos</h1>
+            
+        </>
+    )
+}
